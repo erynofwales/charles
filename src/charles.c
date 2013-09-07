dc558c016be056b9bd60c32eeb18b55bbd71f803
Eryn Wells <eryn@erynwells.me>
2013-09-07 16:10:42 -0700

    Basic driver for the ray tracer

--- src/charles.c
+++ src/charles.c
@@ -5,10 +5,32 @@
  * Eryn Wells <eryn@erynwells.me>
  */
 
+#include <stdio.h>
+
+#include <png.h>
+
+#include "scene.h"
+#include "writer_png.h"
+
+char *OUT_FILE = "charles_out.png";
+
 
 int
 main(int argc,
      const char *argv[])
 {
+    FILE *out_file = fopen(OUT_FILE, "wb");
+    if (!out_file) {
+        return -1;
+    }
+
+    Scene *scene = scene_init();
+
+    scene_render(scene);
+    write_scene_png(scene, out_file);
+
+    scene_destroy(scene);
+    fclose(out_file);
+
     return 0;
 }
