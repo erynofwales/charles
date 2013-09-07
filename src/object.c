8d51a521bb90834632de401de6f848b406a820dd
Eryn Wells <eryn@erynwells.me>
2013-09-07 16:09:19 -0700

    Add object module

---
+++ src/object.c
@@ -0,0 +1,15 @@
+/* object.c
+ *
+ * Definition of scene Objects.
+ *
+ * Eryn Wells <eryn@erynwells.me>
+ */
+
+
+#include "object.h"
+
+
+struct _Object
+{
+    int type;
+};
---
+++ src/object.h
@@ -0,0 +1,16 @@
+/* object.h
+ *
+ * Declaration of scene Objects.
+ *
+ * Eryn Wells <eryn@erynwells.me>
+ */
+
+
+#ifndef __OBJECT_H
+#define __OBJECT_H
+
+
+typedef struct _Object Object;
+
+
+#endif
