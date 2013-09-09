a644539fd3f88700efc1d0248c518a212c60322c
Eryn Wells <eryn@erynwells.me>
2013-09-08 20:52:57 -0700

    TODO: Lighting

--- src/scene.c
+++ src/scene.c
@@ -167,6 +167,8 @@ scene_trace_ray(Scene *scene, const Ray ray, const int depth)
         return out_color;
     }
 
+    // TODO: Lighting.
+
     Texture *tex = object_get_texture(intersected_obj);
     return texture_get_color(tex);
 }
