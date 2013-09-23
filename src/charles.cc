a13ce45d397020ebe30089dc975fcabc1811a582
Eryn Wells <eryn@erynwells.me>
2013-09-22 17:42:50 -0700

    Shift scene objects around to try to see a plane...

--- src/charles.cc
+++ src/charles.cc
@@ -28,34 +28,34 @@ main(int argc,
 
     Material *m1 = new Material();
     m1->set_diffuse_color(Color::Red);
-    m1->set_diffuse_level(0.8);
     Material *m2 = new Material();
     m2->set_diffuse_color(Color::Green);
-    m2->set_diffuse_level(0.8);
     Material *m3 = new Material();
     m3->set_diffuse_color(Color::Blue);
-    m3->set_diffuse_level(0.8);
+    Material *m4 = new Material();
+    m4->set_diffuse_color(Color(1.0, 0.0, 1.0));
 
     // Make some spheres.
-    Sphere *s1 = new Sphere(Vector3(233, 290, 0), 100.0);
-    Sphere *s2 = new Sphere(Vector3(407, 290, 0), 100.0);
-    Sphere *s3 = new Sphere(Vector3(320, 140, 0), 100.0);
+    Sphere *s1 = new Sphere(Vector3(233, 290, 0), 80.0);
+    Sphere *s2 = new Sphere(Vector3(407, 290, 0), 80.0);
+    Sphere *s3 = new Sphere(Vector3(320, 140, 0), 80.0);
+    Sphere *s4 = new Sphere(Vector3(620, 360, 0), 20.0);
     s1->set_material(m1);
     s2->set_material(m2);
     s3->set_material(m3);
+    s4->set_material(m4);
     scene.add_shape(s1);
     scene.add_shape(s2);
     scene.add_shape(s3);
+    scene.add_shape(s4);
 
     // Make a plane
-    Plane *p1 = new Plane(Vector3(0, 420, 0), Vector3::Y);
+    Plane *p1 = new Plane(Vector3(0, 460, 400), Vector3(0, 1, 0.01));
     p1->set_material(m1);
     scene.add_shape(p1);
 
-    PointLight *l1 = new PointLight(Vector3(0.0, 240.0, 100.0), Color(1.0, 1.0, 1.0), 1.0);
-    //Light *l2 = new Light(Vector3(640.0, 240.0, 10000.0), 0.2);
+    PointLight *l1 = new PointLight(Vector3(0.0, 240.0, 100.0), Color::White, 1.0);
     scene.add_light(l1);
-    //scene.add_light(l2);
 
     // Render.
     scene.render();
