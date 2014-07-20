ea43af46bad383d089820fbe416b2ffb0b196f14
Eryn Wells <eryn@erynwells.me>
2014-07-19 21:20:50 -0700

    charles.hh – defines extern'd verbosity

---
+++ src/charles.hh
@@ -0,0 +1,14 @@
+/* charles.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Some basic defines and things.
+ */
+
+#ifndef __CHARLES_HH__
+#define __CHARLES_HH__
+
+extern int verbosity;
+
+#endif /* __CHARLES_HH__ */
