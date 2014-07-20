cf7806484f737059a420d9901d1d2575fd90fa1a
Eryn Wells <eryn@erynwells.me>
2014-07-20 16:53:39 -0700

    Some generic, useful types
    - Double -> double
    - DoubleVector -> std::vector<Double>
    - TVector -> DoubleVector

---
+++ src/types.hh
@@ -0,0 +1,23 @@
+/* types.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+
+#ifndef __CHARLES_TYPES_HH__
+#define __CHARLES_TYPES_HH__
+
+#include <vector>
+
+
+namespace charles {
+
+/* Basic types */
+typedef double Double;
+
+typedef std::vector<Double> DoubleVector;
+typedef DoubleVector TVector;
+
+} /* namespace charles */
+
+#endif /* __CHARLES_TYPES_HH__ */
+
