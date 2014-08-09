a9c8d84ac2be388564efdc597d451d62413d03ab
Eryn Wells <eryn@erynwells.me>
2014-08-08 17:38:13 -0700

    Top-level include for basics

---
+++ src/basics/basics.hh
@@ -0,0 +1,24 @@
+/* basics.hh
+ * vim: set tw=80:
+ * Eryn Wells <eryn@erynwells.me>
+ */
+/**
+ * Top-level include for the basics module.
+ */
+
+#ifndef __BASICS_BASICS_HH__
+#define __BASICS_BASICS_HH__
+
+#include "matrix.hh"
+#include "types.hh"
+
+
+namespace charles {
+namespace basics {
+
+typedef Matrix<4,4> Matrix4;
+
+} /* namespace basics */
+} /* namespace charles */
+
+#endif /* __BASICS_BASICS_HH__ */
