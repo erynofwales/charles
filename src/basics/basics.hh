be15d553f1bf802f75837edd6c12d21c6db635e9
Eryn Wells <eryn@erynwells.me>
2014-08-08 23:04:52 -0700

    Move EPSILON and MAX_DISTANCE to basics/basics.hh

--- src/basics/basics.hh
+++ src/basics/basics.hh
@@ -13,4 +13,17 @@
 #include "basics/types.hh"
 #include "basics/vector.hh"
 
+
+/**
+ * A very small constant. If a value is between EPSILON and 0.0, it is
+ * considered to be zero.
+ */
+const Double EPSILON = 1.0e-10;
+
+/**
+ * The maximum distance a ray can travel. This is the maximum value t can be.
+ */
+const Double MAX_DISTANCE = 1.0e7;
+
+
 #endif /* __BASICS_BASICS_HH__ */
